76766143e0bb8bdb9507edcd0719205b7139b413
sbunkerchris <50494698+sbunkerchris@users.noreply.github.com>
2022-11-12 19:24:00 -0700

    L04Personal

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,6 +13,7 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+const port = 3000 || process.env.PORT
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description',
+  },
+  host: `localhost:${port}`,
+  schemes: ['http'],
+};
+
+console.log('doc start: ' + doc + ' doc end.');
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./app.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
